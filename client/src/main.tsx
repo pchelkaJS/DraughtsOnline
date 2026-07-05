8a46f335046f2b99b8c6edb9435bff68df9ede05
Drakalyebus <mishastelephonom@gmail.com>
2026-07-06 00:06:17 +0300

    baseplate for frontendik

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Hello</h1>
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}>
+
+  </RouterProvider>
 )
